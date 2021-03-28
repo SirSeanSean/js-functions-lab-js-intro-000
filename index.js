5b5f47d7dd563fcb97f91b4612fcbcbe927ee6da
Sean Austin Rosenthal <sean.rosenthal@gloanyx.com>
2021-03-28 18:00:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+//Does not receive any user input
 function happyHolidays()
 {
   return "Happy holidays!"
+}
+
+//The user will input their name and it will spit it back out
+function happyHolidaysTo(name)
+{
+  return "Happy holidays, ${name}!"
 }
